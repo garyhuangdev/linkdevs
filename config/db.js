039ea09ec7a30d938668cc0c1783ba843b9842a7
garyhuangdev <jiazhengsang@gmail.com>
2020-01-17 15:12:30 +1100

    add route

--- config/db.js
+++ config/db.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useUnifiedTopology', true);
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
